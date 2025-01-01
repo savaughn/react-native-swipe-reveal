547fad277d38ec62efcfceef5d3720865e603468
varunkukade <varunkukade999@gmail.com>
2025-01-02 00:01:38 +0530

    feat: ios backward compatibility for turbo module

--- ios/SwipeReveal.h
+++ ios/SwipeReveal.h
@@ -1,6 +1,12 @@
-
+#ifdef RCT_NEW_ARCH_ENABLED
 #import "generated/RNSwipeRevealSpec/RNSwipeRevealSpec.h"
 
 @interface SwipeReveal : NSObject <NativeSwipeRevealSpec>
+#else
+#import <React/RCTBridgeModule.h>
+
+@interface SwipeReveal: NSObject <RCTBridgeModule>
+
+#endif
 
 @end
